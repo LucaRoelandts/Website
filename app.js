03d5b8be489b597d8c282541ba90bc2920538630
60337 <60337@etu.he2b.be>
2023-05-26 11:32:17 +0200

    change

--- app.js
+++ app.js
@@ -3,3 +3,61 @@ const playerDisplay=  document.querySelector("#player")
 const infoDisplay = document.querySelector("#info-display")
 
 const width=8
+
+const startPieces=[
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn, 
+    '', '', '', '', '', '', '', '', 
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+]
+
+function createBoard(){
+    startPieces.forEach((startPieces, index) =>{
+        const square=document.createElement('div')
+        square.classList.add('square')
+        square.innerHTML= startPieces
+        square.firstChild ?.setAttribute('draggable', true)
+        square.setAttribute('square-id', index)
+        const row= Math.floor( (63-index) / 8)+ 1
+        if(row%2==0){
+            square.classList.add(index%2==0 ? "beige" : "brown")
+        }else{
+            square.classList.add(index%2==0 ? "brown" : "beige")
+        }
+        if(index >= 48 ){
+            square.firstChild.classList.add('white')
+        }
+        gameBoard.append(square)
+    })
+}
+createBoard()
+
+
+ const allSquare = document.querySelectorAll("#gameboard .square")
+ 
+ 
+ allSquare.forEach(square => {
+    square.addEventListener('dragstart', dragStart)
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dragDrop)
+ })
+
+ let startPositionId
+ let draggedElement
+
+ function dragStart(e){
+    startpositionId=e.target.parentNode.getAttribute('square-id')
+    draggedElement= e.target
+ }
+
+ function dragOver(e){
+    e.preventDefault()
+ }
+
+ function dragDrop(){
+    
+ }
--- pieces.js
+++ pieces.js
@@ -1,6 +1,6 @@
-const king= "♚"
-const queen="♛"
-const rook="♜"
-const knight="♞"
-const bishop="♝"
-const pon="♟︎"
+const king= '<div class="piece" id="king">♚</div>'
+const queen='<div class="piece" id="queen">♛</div>'
+const rook='<div class="piece" id="rook">♜</div>'
+const knight='<div class="piece" id="knight">♞</div>'
+const bishop='<div class="piece" id="bishop">♝</div>'
+const pawn='<div class="piece" id="pawn">♟︎</div>'
